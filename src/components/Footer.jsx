c07eec117058849adb6ed84362eda43f835b05b9
yuvknightt <ys2554374@gmail.com>
2023-04-08 01:17:10 +0530

    code added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => (
+  <div className="footer">
+    <h2>
+      Made with ❤️ by{" "}
+      <Link to={"https://github.com/yuvknightt"}>YUVRAJ SINGH </Link>{" "}
+    </h2>
+  </div>
+);
+export default Footer;
